0100ee46d8f8ea972ca06688ad773e9f30b3456f
OrtusCattus <145615841+OrtusCattus@users.noreply.github.com>
2023-10-06 08:52:44 -0300

    Update jsHome.js

--- jsHome.js
+++ jsHome.js
@@ -88,21 +88,5 @@ pedidos.forEach(function(pedido) {
 });
 tabelaPedidos.appendChild(tbody);
 
-document.addEventListener("DOMContentLoaded", function () {
-    const themeToggle = document.getElementById("theme-toggle");
-    const body = document.body;
-
-    themeToggle.addEventListener("click", function () {
-        body.classList.toggle("dark-theme");
-    });
-});
-document.addEventListener("DOMContentLoaded", function () {
-    const themeToggle = document.getElementById("theme-toggle");
-    const body = document.body;
-
-    themeToggle.addEventListener("click", function () {
-        body.classList.toggle("dark-mode"); // Altere para "dark-mode"
-    });
-});
 
 
